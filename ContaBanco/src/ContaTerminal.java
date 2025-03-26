5e067fec15d0fe8ceb1d11609c9a9da147a81dad
Guilherme Evangelista <guilhermeev.costa@gmail.com>
2025-03-26 11:28:10 -0300

    ReadMe Projeto Conta Terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -29,7 +29,7 @@ import java.util.Scanner;
         // Imprimindo os dados obtidos pelo usuario
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
         "sua agência é " + agencia + ", conta " + numero + 
-        " e seu saldo " + saldo + 
+        " e seu saldo: " + saldo + 
         " já está disponível para saque.");
 
     }
